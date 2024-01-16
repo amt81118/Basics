e4f82013d1b8daab6a1a7542b0a874f60f7c641c
amt81118 <amityadav81118@gmail.com>
2024-01-16 18:36:35 +0530

    splash screen Android

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View>
       <Text>App</Text>
