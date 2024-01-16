d976e4cc823529d89d3b147d1f6ff7ec84554b40
amt81118 <amityadav81118@gmail.com>
2024-01-16 18:27:00 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
